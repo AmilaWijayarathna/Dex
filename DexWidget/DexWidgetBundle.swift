47ad24d69ab57608c35af5a37ab9f8191bd17001
Amila Wijayarathna <amwijayarathna@gmail.com>
2025-05-25 19:02:48 +0530

    Widget Extention

---
+++ DexWidget/DexWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  DexWidgetBundle.swift
+//  DexWidget
+//
+//  Created by Amila Wijayarathna on 2025-05-25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct DexWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        DexWidget()
+    }
+}
